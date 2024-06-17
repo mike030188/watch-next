41d01cff933553e072b79f770c2bc502db536816
mike030188 <halol.imnida2023@gmail.com>
2024-06-17 18:41:41 +0900

    fix: modify CommunityBoards component

--- libs/components/homepage/CommunityBoards.tsx
+++ libs/components/homepage/CommunityBoards.tsx
@@ -4,6 +4,10 @@ import useDeviceDetect from '../../hooks/useDeviceDetect';
 import { Stack, Typography } from '@mui/material';
 import CommunityCard from './CommunityCard';
 import { BoardArticle } from '../../types/board-article/board-article';
+import { GET_BOARD_ARTICLES } from '../../../apollo/user/query';
+import { useQuery } from '@apollo/client';
+import { T } from '../../types/common';
+import { BoardArticleCategory } from '../../enums/board-article.enum';
 
 const CommunityBoards = () => {
 	const device = useDeviceDetect();
@@ -16,6 +20,36 @@ const CommunityBoards = () => {
 	const [freeArticles, setFreeArticles] = useState<BoardArticle[]>([]);
 
 	/** APOLLO REQUESTS **/
+
+	/** NEWS **/
+	const {
+		loading: getNewsArticlesLoading, // Backendan data kelayotganda Loading... animation korsatadi
+		data: getNewsArticlesData, // datalarni cache saqlayapmiz
+		error: getNewsArticlesError, // data kiriwida error bo`lsa => 41-satr handle | data kirsa "onCompleted" iwga tuwadi
+		refetch: getNewsArticlesRefetch,
+	} = useQuery(GET_BOARD_ARTICLES, {
+		fetchPolicy: 'network-only', // mantiq takroriy iwlab turganda
+		variables: { input: { ...searchCommunity, limit: 6, search: { articleCategory: BoardArticleCategory.NEWS } } }, // POSTMANdagi input
+		notifyOnNetworkStatusChange: true, // by default: false. datalar qayta update bo`lganda iwga tuwadi
+		onCompleted: (data: T) => {
+			setNewsArticles(data?.getBoardArticles?.list);
+		},
+	});
+
+	/** FREE **/
+	const {
+		loading: getFreeArticlesLoading,
+		data: getFreeArticlesData,
+		error: geFreeArticlesError,
+		refetch: getFreeArticlesRefetch,
+	} = useQuery(GET_BOARD_ARTICLES, {
+		fetchPolicy: 'network-only', // mantiq takroriy iwlab turganda
+		variables: { input: { ...searchCommunity, limit: 3, search: { articleCategory: BoardArticleCategory.FREE } } },
+		notifyOnNetworkStatusChange: true,
+		onCompleted: (data: T) => {
+			setFreeArticles(data?.getBoardArticles?.list);
+		},
+	});
 
 	if (device === 'mobile') {
 		return <div>COMMUNITY BOARDS (MOBILE)</div>;
